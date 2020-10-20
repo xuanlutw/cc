23def84d08d24eb8f73217c25bd01413cf378b52
xuan <s986309@gmail.com>
2020-10-21 00:52:59 +0800

    Add regular to NFA

--- reg.c
+++ reg.c
@@ -20,6 +20,94 @@ Regnode* new_regnode(char val) {
     return new_node;
 }
 
+#define TRANS_ENTRY_PER_RECORD 16
+#define NUM_SYMBOLS 128
+#define EPS 0
+typedef struct trans_record {
+    uint16_t             idx[TRANS_ENTRY_PER_RECORD];
+    struct trans_record* next;
+} Trans_record;
+
+typedef struct trans {
+    Trans_record* record[NUM_SYMBOLS];
+} Trans;
+
+typedef struct {
+    Trans*    trans;
+    uint16_t* final_status;
+    uint16_t  init_state;  
+    uint16_t  used_state_s;  
+    uint16_t  used_state_e;  
+} NFA;
+
+Trans_record* new_trans_record() {
+    Trans_record* record = malloc(sizeof(Trans_record));
+    memset(record->idx, 0, sizeof(uint16_t) * TRANS_ENTRY_PER_RECORD);
+    record->next = NULL;
+    return record;
+}
+
+void trans_add(Trans* trans, uint16_t src, uint16_t des, char symbol) {
+    Trans_record* trans_src = trans[src].record[symbol];
+    uint16_t count;
+    if (!trans_src) {
+        trans[src].record[symbol]         = new_trans_record();
+        trans[src].record[symbol]->idx[0] = des;
+        return;
+    }
+    while (true) {
+        for (count = 0; count < TRANS_ENTRY_PER_RECORD; ++count) {
+            if (trans_src->idx[count] == des)
+                return;
+            else if (trans_src->idx[count] == 0) {
+                trans_src->idx[count] = des;
+                return;
+            }
+        }
+        if (!(trans_src->next))
+            break;
+        trans_src = trans_src->next;
+    }
+    trans_src->next        = new_trans_record();
+    trans_src->next->idx[0] = des;
+}
+
+void print_nfa(NFA nfa) {
+    printf("================================\n");
+    printf("init_state: %d\n",   nfa.init_state);  
+    printf("used_state_s: %d\n", nfa.used_state_s);  
+    printf("used_state_e: %d\n", nfa.used_state_e);  
+    for (uint16_t count = nfa.used_state_s; count < nfa.used_state_e; ++count) {
+        bool first_time = true;
+        printf("%3d ", count);
+        for (uint16_t symbol = 0; symbol < NUM_SYMBOLS; ++symbol) {
+            Trans_record* record = nfa.trans[count].record[symbol];
+            if (!record)
+                continue;
+            if (first_time)
+                first_time = false;
+            else
+                printf("    ");
+            printf("%c ", symbol? symbol: ' ');
+            while (record) {
+                for (uint16_t idx = 0; idx < TRANS_ENTRY_PER_RECORD; ++idx)
+                    if (!(record->idx[idx]))
+                        break;
+                    else
+                        printf("%3d ", record->idx[idx]);
+                record = record->next;
+            }
+            printf("\n");
+        }
+        if (first_time)
+            printf("\n");
+    }
+    /*Trans*    trans;*/
+    /*uint16_t* final_status;*/
+    printf("================================\n");
+}
+
+// Stack
 typedef struct {
     void*    val[MAX_REG_LEN + 10];
     uint16_t top_pt;
@@ -66,7 +154,7 @@ void add_node(char val, Stack* stack_node) {
     stack_push(stack_node, node);
 }
 
-Regnode* convert(char* regexp) {
+Regnode* regexp_to_regnode(char* regexp) {
     if (strlen(regexp) > MAX_REG_LEN) {
         printf("Regexp tooooo long\n");
         exit(-1);
@@ -121,6 +209,61 @@ Regnode* convert(char* regexp) {
     return ret;
 }
 
+NFA regnode_to_nfa(Regnode* root, Trans* trans, uint16_t* final_status, \
+                   uint16_t start_state) {
+    uint16_t count;
+    if (root->val == '*') {
+        NFA nfa = regnode_to_nfa(root->left, trans, final_status, start_state);
+        uint16_t init_state  = nfa.used_state_e;
+        uint16_t final_state = nfa.used_state_e + 1;
+        trans_add(trans, init_state, nfa.init_state, EPS);
+        trans_add(trans, init_state, final_state, EPS);
+        for (count = nfa.used_state_s; count < nfa.used_state_e; ++count)
+            if (nfa.final_status[count]) {
+                nfa.final_status[count] = 0;
+                trans_add(trans, count, nfa.init_state, EPS);
+                trans_add(trans, count, final_state, EPS);
+            }
+        nfa.final_status[final_state] = 1;
+        nfa.init_state = init_state;  
+        nfa.used_state_e += 2;  
+        return nfa;
+    }
+    else if (root->val == '|') {
+        NFA nfa1 = regnode_to_nfa(root->left, trans, final_status, start_state);
+        NFA nfa2 = regnode_to_nfa(root->right, trans, final_status, nfa1.used_state_e);
+        uint16_t init_state  = nfa2.used_state_e;
+        trans_add(trans, init_state, nfa1.init_state, EPS);
+        trans_add(trans, init_state, nfa2.init_state, EPS);
+        nfa1.init_state = init_state;  
+        nfa1.used_state_e = nfa2.used_state_e + 1;  
+        return nfa1;
+    }
+    else if (root->val == '.') {
+        NFA nfa1 = regnode_to_nfa(root->left, trans, final_status, start_state);
+        NFA nfa2 = regnode_to_nfa(root->right, trans, final_status, nfa1.used_state_e);
+        for (count = nfa1.used_state_s; count < nfa1.used_state_e; ++count)
+            if (nfa1.final_status[count]) {
+                nfa1.final_status[count] = 0;
+                trans_add(trans, count, nfa2.init_state, EPS);
+            }
+        nfa1.used_state_e = nfa2.used_state_e;
+        return nfa1;
+    }
+    else {
+        NFA nfa = {
+            .trans        = trans,
+            .final_status = final_status,
+            .init_state   = start_state,  
+            .used_state_s = start_state,
+            .used_state_e = start_state + 2
+        };
+        trans_add(trans, start_state, start_state + 1, root->val);
+        final_status[start_state + 1] = 1;
+        return nfa;
+    }
+}
+
 void print(Regnode* root) {
     if (!root)
         return;
@@ -133,11 +276,19 @@ int main() {
     /*Regnode* S = convert("(a|b)*.a.b.b.#");*/
     /*Regnode* S = convert("(0|(1.(0.1*.(0.0)*.0)*.1)*)*");*/
     /*Regnode* S = convert("(a|b.c)*");*/
-    /*Regnode* S = convert("(a|bc)*");*/
-    Regnode* S = convert("(0|(1(01*(00)*0)*1)*)*");
-    /*Regnode* S = convert("(a|b)*abb#");*/
+    /*Regnode* S = regexp_to_regnode("(a|bc)*");*/
+    /*Regnode* S = regexp_to_regnode("(0|(1(01*(00)*0)*1)*)*");*/
+    Regnode* S = regexp_to_regnode("(a|b)*abb#");
     print(S);
     printf("\n");
+
+    Trans    trans[MAX_REG_LEN];
+    uint16_t final_status[MAX_REG_LEN];
+    memset(trans, 0, sizeof(Trans) * MAX_REG_LEN);
+    memset(final_status, 0, sizeof(uint16_t) * MAX_REG_LEN);
+    NFA nfa = regnode_to_nfa(S, trans, final_status, 1);
+    print_nfa(nfa);
+
     return 0;
 }
 
